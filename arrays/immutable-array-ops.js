c42b8674ea6da7c4adabea37a02da7cc3f0ab1bf
developer <renewdigital>
2020-09-22 15:56:32 +1000

    started array utils

--- arrays/immutable-array-ops.js
+++ arrays/immutable-array-ops.js
@@ -1,19 +1,19 @@
 /* INSERT element at index */
-export default function insertAtIndex(index, item, array) {
+export function insertAtIndex(index, item, array) {
     return [...array.slice(0, index + 1), item, ...array.slice(index + 1)]
 }
 
 /* REMOVE element at index */
-export default function removeAtIndex(indexToRemove, array) {
+export function removeAtIndex(indexToRemove, array) {
     return [...array.slice(0, indexToRemove), ...array.slice(indexToRemove + 1)];
 }
 
 /* APPEND value to end of array */
-export default function appendToEnd(item, array) {
+export function appendToEnd(item, array) {
     return [...array, item];
 }
 
 /* PREPEND value to start of array */
-export default function prependToStart(item, array) {
+export function prependToStart(item, array) {
     return [item, ...array];
 }
